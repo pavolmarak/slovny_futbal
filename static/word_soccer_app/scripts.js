b5209ffd004d8fc4da72d96cc90dd977c6f0cce0
pavolmarak <pavol.marak@gmail.com>
2025-08-26 17:21:55 +0200

    Working on letter highlight removal.

--- static/word_soccer_app/scripts.js
+++ static/word_soccer_app/scripts.js
@@ -7,25 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const removerDiv = document.getElementById('remover');
     const submitWord = document.getElementById('submit_word');
 
+    let order_in_word;
+
+    if (localStorage.getItem("order_in_word") !== null) {
+        order_in_word = new Array(11).fill(-1)
+        localStorage.setItem("order_in_word", JSON.stringify(order_in_word));
+    } else {
+        order_in_word = JSON.parse(localStorage.getItem("order_in_word")) || [];
+    }
 
     document.querySelectorAll('.let').forEach(one_letter => {
         one_letter.addEventListener('click', function () {
             if(one_letter.style.backgroundColor !== "red"){
                 wordDiv.textContent += one_letter.textContent;
                 one_letter.style.backgroundColor = "red";
+                let order = one_letter.id.slice(1);
+                order_in_word[Number(order)-1] = wordDiv.textContent.length-1;
             }
         });
     });
 
-
     deleterDiv.addEventListener('click', function () {
         const div = document.getElementById('word');
+        let idx = order_in_word.indexOf(div.textContent.length-1);
+        order_in_word[idx] = -1;
+        document.getElementById('l'+ (idx+1).toString()).style.backgroundColor = "#359ec4";
         div.textContent = div.textContent.slice(0, -1);
     });
 
     removerDiv.addEventListener('click', function () {
         document.getElementById('word').textContent = '';
-
         document.querySelectorAll('.let').forEach(one_letter => {
             one_letter.style.backgroundColor = "#359ec4";
          });
