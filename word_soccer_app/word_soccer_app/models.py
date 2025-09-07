d29f810e5ce7fba88e98e4b64b4d6e3158b8dde8
pavolmarak <pavol.marak@gmail.com>
2025-09-07 12:08:47 +0200

    Added new models to database.

--- word_soccer_app/word_soccer_app/models.py
+++ word_soccer_app/word_soccer_app/models.py
@@ -2,16 +2,46 @@ from django.db import models
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True)  # idle, waiting, playing
 
     def __str__(self):
         return self.name
 
 
+class MatchStatus(models.Model):
+    in_progress = models.BooleanField(default=False)
+    goals_player1 = models.IntegerField(default=0)
+    goals_player2 = models.IntegerField(default=0)
+    has_player1_yellow_card = models.BooleanField(default=False)
+    has_player2_yellow_card = models.BooleanField(default=False)
+    red_cards_player1 = models.IntegerField(default=0)
+    red_cards_player2 = models.IntegerField(default=0)
+
+
+class Match(models.Model):
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='matches_as_player1', null=True)
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='matches_as_player2', null=True)
+    status = models.ForeignKey(MatchStatus, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.player1.name + ' vs. ' + self.player2.name
+
+
+class Round(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    in_progress = models.BooleanField(default=False)
+    letters_player1 = models.IntegerField(default=0)
+    letters_player2 = models.IntegerField(default=0)
+
+
 class Word(models.Model):
-    text = models.CharField(max_length=200)
-    player_id = models.IntegerField(default=0)
-    is_in_dict = models.BooleanField(default=False)
+    round = models.ForeignKey(Round, on_delete=models.CASCADE, null=True)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=200, null=True)
+    is_in_dict = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.text
+
+
--- word_soccer_app/word_soccer_app/urls.py
+++ word_soccer_app/word_soccer_app/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('find/', views.find_word, name='find_word')
 ]
